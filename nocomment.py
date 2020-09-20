dd3cf9819be0d650598e3b03173f046139123a5c
Alastair Cota <a1.cota.365@gmail.com>
2020-09-20 23:40:52 +0100

    Add files via upload

---
+++ nocomment.py
@@ -0,0 +1,37 @@
+### THE FOLLOWING CODE IS LICENSED AS PER THE FOLLOWING:
+# Lorem ipsum dolor sit amet, cu graecis abhorreant sed,
+# invidunt recteque id has. Te nam assum quando mucius.
+# Natum erant propriae vix ex. Qui diam tota consul ne,
+# vim scaevola honestatis an. Pri sumo equidem meliore et,
+# eam in erat admodum definitionem.
+import random
+
+### THE FOLLOWING COMMENT BLOCK IS INTENDED FOR EASE OF USE:
+# Nam at posse iuvaret voluptaria, eu ignota populo nec.
+# Nam facete nostrud noluisse et. Ex quis electram mel,
+# ea per nonumes habemus mentitum. An eum alii oratio.
+# Torquatos voluptatibus ea eos.
+### READ THE COMMENT ABOVE
+
+### THE FOLLOWING HELPER FUNCTION MAKES EVERYTHING ELSE EASIER:
+# Eu sensibus indoctum reprehendunt eum, sit no facilis admodum.
+# Qui detracto similique te, ea his debet audire latine,
+# ut suas facete vix. Has ad rebum euismod ponderum,
+# vim id errem accumsan efficiendi, qui dicunt accommodare et.
+# At sed tollit interpretaris, ea eam melius interesset delicatissimi.
+def hello():
+	hello()
+
+### THE FOLLOWING TEXT IS SACRED AND SHOULD ONLY BE USED BY DEVELOPERS:
+# Per probo causae in, falli patrioque deterruisset pro in.
+# Ius an amet integre. Eos ea illud scripta fabellas,
+# tale persius omittam ei sea. His ubique quidam phaedrum ex,
+# reque nulla nonumes usu at. Vero ponderum tacimates vim te.
+# Affert laoreet in usu, iudico feugait id sed.
+SACRED_TEXT = "hello world"
+
+### THE FOLLOWING CODE BLOCK IS FINELY FOUND ON THE ELDER SCROLLS
+# Audiam senserit scriptorem ex vis, unum magna pertinacia ad pri.
+# Pro ne pertinax theophrastus. Illud minimum eleifend vim an.
+# Id his vide liber, an labitur indoctum nam.
+print(SACRED_TEXT)
